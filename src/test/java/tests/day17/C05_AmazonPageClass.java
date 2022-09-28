66cfe5c5829cbe6f9bda47974aac5af6f319ffa5
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-09-28 23:26:40 +0300

    V3

---
+++ src/test/java/tests/day17/C05_AmazonPageClass.java
@@ -0,0 +1,8 @@
+package tests.day17;
+
+import org.testng.annotations.Test;
+
+public class C05_AmazonPageClass {
+
+
+}
---
+++ src/test/java/tests/review/AllertSoruOrnegi.java
@@ -0,0 +1,9 @@
+package tests.review;
+
+public class AllertSoruOrnegi {
+    // 1. "http://webdriveruniversity.com/Popup-Alerts/index.html" adresine gidin
+// 2. JavaScript alertin "CLICK ME!" seçeneğine tıklayın
+// 3. Açılır metni alın
+// 4. Mesajın "I am an alert box!" olduğunu doğrulayın.
+// 5. Açılır pencereyi kabul edin
+}
