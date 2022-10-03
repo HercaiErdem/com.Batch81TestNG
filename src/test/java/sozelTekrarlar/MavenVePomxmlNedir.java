520d42e29fdb1959066924c410cba08710fa9520
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-04 00:28:36 +0300

    4.10.2022 V1

---
+++ src/test/java/sozelTekrarlar/MavenVePomxmlNedir.java
@@ -0,0 +1,80 @@
+package sozelTekrarlar;
+
+public class MavenVePomxmlNedir {
+
+    /*
+    Herkese Merhabalar,
+    Bugün, Selenium da daha iyi proje yönetimi yapmamızı sağlayan Apache Maven ile ilgili
+    aldığım notları paylaşmak istiyorum.
+    */
+
+    // İlk öncelikle Maven Nedir?
+    /*
+    * Maven, daha iyi proje yönetimi(effective project management) için aynı anda birkaç proje inşa etmek(build),
+      yayınlamak(publish) için Apache Group tarafından geliştirilen son dönemlerde yaygınca kullanılan
+      bir açık kaynaklı(open source) otomasyon aracıdır(automation tool).
+
+    * Kısacası, Apache Maven bir yazılım proje yönetimi ve anlama tool udur.
+
+    * Maven, Proje nesne modeli (POM Project Object Model) konseptine dayalidir.
+
+    * Maven geliştiricileri bizi birçok ayrıntıdan korur. Çünkü çoğu IDE(intelliJ, eclipse, vb.), Maven için eklenti(plugin) desteği sağlayarak kolayca
+     Maven projesi oluşturmanızı sağlar. Eğer Maven kullanmazsak da proje geliştirirken çeşitli kütüphaneler kullanmak
+     isteyebiliriz. Her kütüphane için gereken .jar dosyalarını tek tek indirip projeye uygun olarak yerleştirmemiz(classpath) gerekir.
+     */
+
+    // Kısacası Maven Tool unu Tercih Etmemizin Sebebleri;
+    /*
+    *Proje yönetimini kolaylaştırır.
+    *Open source
+    *Kullanıcı sayısı fazladır
+    * Maven, dependencylerle ilgili mevcut jar dosyalarını otomatik olarak indirir.
+    *Birden fazla IDE (intelliJ, eclipse, vb.) ve araçlarla(tools) çalışır.
+    * Yeniliklere geçişi kolaydır. O yüzden, Tekrarlanabilir derlemeler / yürütmeleri yapmak daha basittir.
+
+
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
