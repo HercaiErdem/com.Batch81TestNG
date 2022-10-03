c3b7b018a6c801411213882240316ccb45a0e471
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-03 18:08:04 +0300

    V5

--- src/test/java/tests/day17/C01_DependsOnMethods.java
+++ src/test/java/tests/day17/C01_DependsOnMethods.java
@@ -6,7 +6,7 @@ import utilities.TestBaseBeforeMethodAfterMethod;
 public class C01_DependsOnMethods extends TestBaseBeforeMethodAfterMethod {
     @Test
     public void test01() {
-        driver.get("https://www.wamazon1.com");
+        driver.get("https://www.amazon1.com");
 
     }
 
