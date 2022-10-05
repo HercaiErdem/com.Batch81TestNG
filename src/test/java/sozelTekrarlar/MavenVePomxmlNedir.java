5ac76acb0c4ad33079652c4f55203e686aec6e3f
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-05 09:54:30 +0300

    5.10.2022 V1

--- src/test/java/sozelTekrarlar/MavenVePomxmlNedir.java
+++ src/test/java/sozelTekrarlar/MavenVePomxmlNedir.java
@@ -19,18 +19,18 @@ public class MavenVePomxmlNedir {
     * Maven, Proje nesne modeli (POM Project Object Model) konseptine dayalidir.
 
     * Maven geliştiricileri bizi birçok ayrıntıdan korur. Çünkü çoğu IDE(intelliJ, eclipse, vb.), Maven için eklenti(plugin) desteği sağlayarak kolayca
-     Maven projesi oluşturmanızı sağlar. Eğer Maven kullanmazsak da proje geliştirirken çeşitli kütüphaneler kullanmak
-     isteyebiliriz. Her kütüphane için gereken .jar dosyalarını tek tek indirip projeye uygun olarak yerleştirmemiz(classpath) gerekir.
+     Maven projesi oluşturmanızı sağlar. Eğer Maven kullanmazsak  proje geliştirirken çeşitli kütüphaneler kullanmak
+     zorunda kalabiliriz. Her kütüphane için gereken .jar dosyalarını tek tek indirip projeye uygun olarak yerleştirmemiz(classpath) gerekir.
      */
 
     // Kısacası Maven Tool unu Tercih Etmemizin Sebebleri;
     /*
     *Proje yönetimini kolaylaştırır.
-    *Open source
+    *Open source olması
     *Kullanıcı sayısı fazladır
-    * Maven, dependencylerle ilgili mevcut jar dosyalarını otomatik olarak indirir.
-    *Birden fazla IDE (intelliJ, eclipse, vb.) ve araçlarla(tools) çalışır.
-    * Yeniliklere geçişi kolaydır. O yüzden, Tekrarlanabilir derlemeler / yürütmeleri yapmak daha basittir.
+    *Maven, dependencylerle ilgili mevcut jar dosyalarını otomatik olarak indirmesi
+    *Birden fazla IDE (intelliJ, eclipse, vb.) ve araçlarla(tools) çalışması
+    *Yeniliklere geçişinin kolay olması. O yüzden de Tekrarlanabilir derlemeler / yürütmeleri yapmak daha basittir.
 
 
      */
