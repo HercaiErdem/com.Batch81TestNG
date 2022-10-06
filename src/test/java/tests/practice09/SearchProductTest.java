cd12558a6e0d2bd05fb450ff361ca896fe0708df
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-06 19:00:25 +0300

    V3

--- src/test/java/tests/practice09/SearchProductTest.java
+++ src/test/java/tests/practice09/SearchProductTest.java
@@ -25,18 +25,25 @@ public class SearchProductTest extends TestBaseRapor {
 
     @Test
     public void searchProductPage() throws InterruptedException {
+        extentTest=extentReports.createTest("automationexcercise","Web automation");
 
         // 1. Tarayıcıyı başlat
         // 2. 'http://automationexercise.com' url'sine gidin
+        extentTest.info("   // 1. Tarayıcıyı başlat\n" +
+                "        // 2. 'http://automationexercise.com' url'sine gidin");
+
         Driver.getDriver().get("http://automationexercise.com");
         // 3. Ana sayfanın başarıyla görünür olduğunu doğrulayın
         String expectedUrl = "https://automationexercise.com/";
         Assert.assertEquals(Driver.getDriver().getCurrentUrl(), expectedUrl);
+        extentTest.info("3. Ana sayfanın başarıyla görünür olduğunu doğrulayın");
         // 4. 'Ürünler' butonuna tıklayın
         searchProductPage = new SearchProductPage();
         searchProductPage.products.click();
+        extentTest.info("4. 'Ürünler' butonuna tıklayın");
         // 5. Kullanıcının TÜM ÜRÜNLER sayfasına başarıyla gittiğini doğrulayın
         Assert.assertTrue(searchProductPage.allProductTitle.isDisplayed());
+        extentTest.info("5. Kullanıcının TÜM ÜRÜNLER sayfasına başarıyla gittiğini doğrulayın");
         // 6. Arama girişine ürün adını girin ve ara düğmesine tıklayın
         searchProductPage.searchBox.sendKeys("blue top");
         // sendKeys("Dress", Keys.TAB,Keys.ENTER) ---> boyle de calisir
@@ -46,8 +53,8 @@ public class SearchProductTest extends TestBaseRapor {
         // 8. Aramayla ilgili ürünün ("blue top") görünür olduğunu doğrulayın
         Thread.sleep(2000);
         actions.click(searchProductPage.viewProduct).perform();
-        Assert.assertTrue(searchProductPage.blueTop.isDisplayed());
-
+         Assert.assertTrue(searchProductPage.blueTop.isDisplayed());
+        extentTest.pass("Aramayla ilgili ürünün (\"blue top\") görünür olduğu doğrulandı");
 
     }
 
