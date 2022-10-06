0a98a6908b543f2e087fb0a5e80dfc4a91afef6d
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-06 21:49:25 +0300

    V5

--- src/test/java/tests/day23/C01_DataProvider.java
+++ src/test/java/tests/day23/C01_DataProvider.java
@@ -2,32 +2,33 @@ package tests.day23;
 
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class C01_DataProvider {
-    @Test
-    public void test01() {
-     //   AmazonPage amazonPage = new AmazonPage();
-        //Amazon sayfasına gidelim
-        Driver.getDriver().get(ConfigReader.getProperty("amznUrl"));
-        //Nutella için arama yapalım
-       // amazonPage.aramaKutusu.sendKeys("Nutella", Keys.ENTER);
-        //sonuclarin nutella icerdigini test edelim
-        String expectedKelime = "Nutella";
-       // String actualKelime = amazonPage.aramaSonucWE.getText();
-        // Assert.assertTrue(actualKelime.contains(expectedKelime));
-        Driver.closeDriver();
+
+
+    @DataProvider
+    public static Object[][] aranacakKelimeler() {
+        return new Object[][]{{"java"}, {"selenium"}, {"samsung"}, {"iphone"}};
     }
-    @Test
-    public void test02() {
-        // AmazonPage amazonPage = new AmazonPage();
+
+    @Test(dataProvider = "aranacakKelimeler")
+    public void test02(String kelimeler) {
+        //   AmazonPage amazonPage = new AmazonPage();
         //amazona gidelim
         Driver.getDriver().get(ConfigReader.getProperty("amznUrl"));
         //java, selenium, samsung ve iphone icin arama yapalim
+        //  amazonPage.aramaKutusu.sendKeys(kelimeler, Keys.ENTER);
         //sonuclarin aradigimiz kelime icerdigini test edelim
-        //sayfayi kapatalim
+        //  String actualKelime = amazonPage.aramaSonucWE.getText();
+        //  Assert.assertTrue(actualKelime.contains(kelimeler));
+    }
+
+    @Test(dependsOnMethods = "test02")
+    public void test03() {
         Driver.closeDriver();
     }
 }
