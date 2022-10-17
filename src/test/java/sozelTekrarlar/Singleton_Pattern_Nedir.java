ff61a6c93f4ed99f48beacf55398234ba9f553bc
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-17 18:12:59 +0300

    17.10.2022 V1

---
+++ src/test/java/sozelTekrarlar/Singleton_Pattern_Nedir.java
@@ -0,0 +1,21 @@
+package sozelTekrarlar;
+
+public class Singleton_Pattern_Nedir {
+
+    // Singleton Pattern (Tekli Kullanim)
+    /*
+    ->Singleton pattern(tekli kullanım) ile herhangi bir Java classindan obje kullanımını
+    sınırlayabılırız.
+    ->Singleton, bir class'ın yalnızca tek bir instance'a sahip olmasını sağlarken
+    bu instance'a genel bir erişim noktası sağlamamıza olanak tanıyan creational
+    bir software dizayn kalıbıdır.
+    ->Proje genelinde farklı objeler olusturulmadan tek bir instance gereksinimimiz olduğunda
+    Singleton class kullaniriz.
+    ->Ayrıca Singleton, bir objeyi tekrar tekrar oluşturmayı engellediği için performans ve
+     bellek kullanımı için de faydalıdır
+
+
+     */
+
+
+}
