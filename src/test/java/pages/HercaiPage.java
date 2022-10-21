41ff2c48180b1ab687f1dee0db494b03b0cf2ac5
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-21 15:15:56 +0300

    v2

--- src/test/java/pages/HercaiPage.java
+++ src/test/java/pages/HercaiPage.java
@@ -1,4 +1,40 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class HercaiPage {
+    public HercaiPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//span[.='Sign In']")
+    public WebElement singin;
+
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement email;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//button[@name='login']")
+    public WebElement singinIkınci;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/tests/hercai/US010_TC001.java
+++ src/test/java/tests/hercai/US010_TC001.java
@@ -1,4 +1,60 @@
 package tests.hercai;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.HercaiPage;
+import utilities.Driver;
+
 public class US010_TC001 {
+    //US-Renk secenegi; beyaz, siyah, yeşil vs tum renkler belirtilmeli
+
+    //Belirtilen URL` ye gidilir "https://allovercommerce.com/"
+    //Sing in butonuna tıklanır
+    //Username or email adress bölümüne vendor email girilir "grup20.testng@gmail.com"
+    //Password girilir "Vendor20.,"
+    //Sing In butonuna tıklanır
+    //Sayfanın altından My Account butonuna tıklanır
+    //Store Manager bölümüne tıklanır
+    //Products yazısına tıklanır
+    //Add New butonuna tıklanır
+    //Aşağıda Attributes kutusuna tıklanır
+    //Color yazısının yanındaki kutucuğa tıklanır
+    //Aşağı oka tıklanır
+    //Select All kutusuna basılır
+    //Tum renklerin seçildiği doğrulanır
+    //SUBMIT butonuna tıklanır
+
+    HercaiPage hercaiPage = new HercaiPage();
+    Actions actions = new Actions(Driver.getDriver());
+
+    @Test
+    public void test01() {
+        //Belirtilen URL` ye gidilir "https://allovercommerce.com/"
+        Driver.getDriver().get("https://allovercommerce.com/");
+        //Sing in butonuna tıklanır
+        hercaiPage.singin.click();
+
+        //Username or email adress bölümüne vendor email girilir "grup20.testng@gmail.com"
+        hercaiPage.email.sendKeys("grup20.testng@gmail.com");
+        //Password girilir "Vendor20.,"
+        //Sing In butonuna tıklanır
+        actions.sendKeys(Keys.TAB).sendKeys(hercaiPage.password, "Vendor20.,")
+                .sendKeys(Keys.TAB).click(hercaiPage.singinIkınci).perform();
+        //Sayfanın altından My Account butonuna tıklanır
+
+        //Store Manager bölümüne tıklanır
+        //Products yazısına tıklanır
+        //Add New butonuna tıklanır
+        //Aşağıda Attributes kutusuna tıklanır
+        //Color yazısının yanındaki kutucuğa tıklanır
+        //Aşağı oka tıklanır
+        //Select All kutusuna basılır
+        //Tum renklerin seçildiği doğrulanır
+        //SUBMIT butonuna tıklanır
+
+
+    }
+
+
 }
