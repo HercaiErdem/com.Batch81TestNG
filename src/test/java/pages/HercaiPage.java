ef372ade2b3bc6a0fee65109842a8cc14fa3d641
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-21 16:10:38 +0300

    v3

--- src/test/java/pages/HercaiPage.java
+++ src/test/java/pages/HercaiPage.java
@@ -22,19 +22,35 @@ public class HercaiPage {
     @FindBy(xpath = "//button[@name='login']")
     public WebElement singinIkınci;
 
+    @FindBy(xpath = "(//a[@href='https://allovercommerce.com/my-account-2/'])[1]")
+    public WebElement myAccount;
 
+    @FindBy(xpath = "//a[text()='Store Manager']")
+    public WebElement storeManager;
 
+    @FindBy(xpath = "(//span[@class='text'])[4]")
+    public WebElement products;
 
+    @FindBy(xpath = "(//*[.='Add New'])[1]")
+    public WebElement addNew;
 
+    @FindBy(xpath = "(//div[@class='page_collapsible_content_holder'])[5]")
+    public WebElement attributies;
 
+    @FindBy(xpath = "//input[@id='attributes_is_active_1']")
+    public WebElement colorBox;
 
+    @FindBy(xpath = "(//ul[@class='select2-selection__rendered'])[2]")
+    public WebElement colorBigBox;
 
+    @FindBy(xpath = "(//button[text()='Select all'])[1]")
+    public WebElement selectAll;
 
+    @FindBy(xpath = "(//ul[@class='select2-selection__rendered'])[2]")
+    public WebElement allColorSelected;
 
-
-
-
-
+    @FindBy(xpath = "//input[@id='wcfm_products_simple_submit_button']")
+    public WebElement submitButton;
 
 
 }
--- src/test/java/tests/hercai/US010_TC001.java
+++ src/test/java/tests/hercai/US010_TC001.java
@@ -1,10 +1,15 @@
 package tests.hercai;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import pages.HercaiPage;
 import utilities.Driver;
+
+import java.time.Duration;
 
 public class US010_TC001 {
     //US-Renk secenegi; beyaz, siyah, yeşil vs tum renkler belirtilmeli
@@ -27,9 +32,11 @@ public class US010_TC001 {
 
     HercaiPage hercaiPage = new HercaiPage();
     Actions actions = new Actions(Driver.getDriver());
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+    JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
 
     @Test
-    public void test01() {
+    public void test01() throws InterruptedException {
         //Belirtilen URL` ye gidilir "https://allovercommerce.com/"
         Driver.getDriver().get("https://allovercommerce.com/");
         //Sing in butonuna tıklanır
@@ -42,15 +49,29 @@ public class US010_TC001 {
         actions.sendKeys(Keys.TAB).sendKeys(hercaiPage.password, "Vendor20.,")
                 .sendKeys(Keys.TAB).click(hercaiPage.singinIkınci).perform();
         //Sayfanın altından My Account butonuna tıklanır
+        Driver.getDriver().navigate().refresh();
+      //  actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
+        wait.until(ExpectedConditions.visibilityOf(hercaiPage.myAccount));
+     //   hercaiPage.myAccount.click();
+         jse.executeScript("arguments[0].scrollIntoView(true);", hercaiPage.myAccount);
+         jse.executeScript("arguments[0].click();",hercaiPage.myAccount);
 
         //Store Manager bölümüne tıklanır
+     //   hercaiPage.storeManager.click();
         //Products yazısına tıklanır
+
         //Add New butonuna tıklanır
+
         //Aşağıda Attributes kutusuna tıklanır
+
         //Color yazısının yanındaki kutucuğa tıklanır
+
         //Aşağı oka tıklanır
+
         //Select All kutusuna basılır
+
         //Tum renklerin seçildiği doğrulanır
+
         //SUBMIT butonuna tıklanır
 
 
