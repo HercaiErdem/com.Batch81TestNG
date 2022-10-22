312bd51d2cdd4f0b8affe4b1ff8a4f83d5a920e2
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-22 19:08:18 +0300

    v3

--- src/test/java/tests/hercai/US020_TC001.java
+++ src/test/java/tests/hercai/US020_TC001.java
@@ -1,4 +1,60 @@
 package tests.hercai;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import pages.HercaiPage;
+import utilities.Driver;
+
+import java.time.Duration;
+
 public class US020_TC001 {
+    // Kullanici bilgisi, yazmış olduğu comment
+    // Belirtilen URL` ye gidilir
+    //"Sing in" butonuna tıklanır
+    //"Username or email adress" bölümüne vendor email girilir
+    //"Password" bölümüne verdor password girilir
+    //"Sing in" butonuna tıklanır
+    //Açılan sayfanın alt kısmında bulunan "My Account" butonuna tıklanır
+    //"Store Manager" butonuna tıklanır
+    //Reviews butonuna tıklanır
+    //Products Reviews kutucuğuna tıklanır
+    //"tarikcustomer
+    //trkklckya@hotmail.com yazısının gözüktüğü doğrulanır"
+    //kullanışlı ve rahat bir ürün, teşekkür ederim. yazının göründüğü doğrulanır
+
+    HercaiPage hercaiPage = new HercaiPage();
+    Actions actions = new Actions(Driver.getDriver());
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+    JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+
+    @Test
+    public void test01() {
+        // Belirtilen URL` ye gidilir
+        Driver.getDriver().get("https://allovercommerce.com/");
+        //"Sing in" butonuna tıklanır
+        hercaiPage.singin.click();
+        //"Username or email adress" bölümüne vendor email girilir
+        hercaiPage.email.sendKeys("grup20.testng@gmail.com");
+        //"Password" bölümüne verdor password girilir
+        //"Sing in" butonuna tıklanır
+        actions.sendKeys(Keys.TAB).sendKeys(hercaiPage.password, "Vendor20.,")
+                .sendKeys(Keys.TAB).click(hercaiPage.singinIkınci).perform();
+        //Açılan sayfanın alt kısmında bulunan "My Account" butonuna tıklanır
+        Driver.getDriver().navigate().refresh();
+        wait.until(ExpectedConditions.visibilityOf(hercaiPage.myAccount));
+        jse.executeScript("arguments[0].scrollIntoView(true);", hercaiPage.myAccount);
+        jse.executeScript("arguments[0].click();", hercaiPage.myAccount);
+        //"Store Manager" butonuna tıklanır
+        //Reviews butonuna tıklanır
+        //Products Reviews kutucuğuna tıklanır
+        //"tarikcustomer
+        //trkklckya@hotmail.com yazısının gözüktüğü doğrulanır"
+        //kullanışlı ve rahat bir ürün, teşekkür ederim. yazının göründüğü doğrulanır
+
+
+    }
 }
