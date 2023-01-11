d9aa73d380c1a779dbac1aa4a09bf70bb50b96fa
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-11 22:25:53 +0300

    v3

--- src/test/java/tests/day22/C02_HardAssert.java
+++ src/test/java/tests/day22/C02_HardAssert.java
@@ -1,12 +1,9 @@
 package tests.day22;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import utilities.TestBaseBeforeMethodAfterMethod;
-import utilities.TestBaseCross;
+import org.openqa.selenium.*;
+import org.testng.*;
+import org.testng.annotations.*;
+import utilities.*;
 
 public class C02_HardAssert extends TestBaseCross {
 
@@ -15,7 +12,7 @@ public class C02_HardAssert extends TestBaseCross {
 // Hard Assertions'da herhangi bir assertion failed olursa,
 // test method'un çalışmasını durdurur ve kalan kodları yürütmez.(stop execution)
 
-// Soft Assert : Kullanabilmek için obje oluştururuz, oluşturduktan sonra obje ismi ile kullanırız,
+// Soft Assert(Verification) : Kullanabilmek için obje oluştururuz, oluşturduktan sonra obje ismi ile kullanırız,
 // Herhangi bir hata verdiğinde test methodu
 // çalışmaya devam eder, en sonda rapor verir. baslangıc  SoftAssert objeadı=new SoftAssert();
 // bitis objeadı.assertAll();
