8064df78b459bdce4122fb53e31a09b735b92107
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-11 22:37:11 +0300

    v12

--- src/test/java/utilities/TestBaseBeforeClassAfterClass.java
+++ src/test/java/utilities/TestBaseBeforeClassAfterClass.java
@@ -1,13 +1,10 @@
 package utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-
-import java.time.Duration;
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.testng.annotations.*;
+import java.time.*;
 
 public abstract class TestBaseBeforeClassAfterClass {
 
