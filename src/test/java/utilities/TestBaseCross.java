915c00e05c4734ec3ba6fc3f264b9d7c333d3673
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-11 22:38:44 +0300

    v14

--- src/test/java/utilities/TestBaseCross.java
+++ src/test/java/utilities/TestBaseCross.java
@@ -1,13 +1,8 @@
 package utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import java.time.Duration;
+import org.openqa.selenium.*;
+import org.testng.annotations.*;
+import java.time.*;
 public class TestBaseCross {
     protected WebDriver driver;
     @Parameters("browser")
