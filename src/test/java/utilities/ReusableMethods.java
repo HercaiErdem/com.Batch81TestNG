d6ff2aa7abf3e619328a637323a7c27420f41306
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-11 22:36:32 +0300

    v11

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,23 +1,13 @@
 package utilities;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.function.Function;
+import org.openqa.selenium.interactions.*;
+import org.openqa.selenium.support.ui.*;
+import java.io.*;
+import java.text.*;
+import java.time.*;
+import java.util.*;
 
 public class ReusableMethods {
     //========ScreenShot(Sayfanın resmini alma)=====//
