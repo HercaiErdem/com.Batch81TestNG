5de9efe22dd60ebaae5b1488d8cc17b6ea512db8
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-06 19:42:41 +0300

    v3

--- src/test/java/tests/day18/C01_PageClassFacebookTest.java
+++ src/test/java/tests/day18/C01_PageClassFacebookTest.java
@@ -10,31 +10,20 @@ public class C01_PageClassFacebookTest {
 
     @Test
     public void test01() {
+
         // https://www.facebook.com/ adresine gidin
         Driver.getDriver().get("https://www.facebook.com/");
         //POM’a uygun olarak email, sifre kutularini ve giris yap
         //butonunu locate edin
         FacebookPage facebookPage = new FacebookPage();
         //Faker class’ini kullanarak email ve sifre degerlerini
-        Faker faker= new Faker();
+        Faker faker = new Faker();
         //yazdirip, giris butonuna basin
         facebookPage.email.sendKeys(faker.internet().emailAddress());
         facebookPage.sifre.sendKeys(faker.internet().password());
         facebookPage.login.click();
         //Basarili giris yapilamadigini test edin
         Assert.assertTrue(facebookPage.girisYapilmadi.isDisplayed());
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
