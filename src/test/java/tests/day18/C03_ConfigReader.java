5725e11e392014d9f6773ef3d684442f714a0350
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-06 19:47:54 +0300

    v6

--- src/test/java/tests/day18/C03_ConfigReader.java
+++ src/test/java/tests/day18/C03_ConfigReader.java
@@ -11,13 +11,16 @@ import utilities.Driver;
 public class C03_ConfigReader {
     @Test
     public void test01() {
+
         //  https://www.hotelmycamp.com/ adresine git  login butonuna bas
         Driver.getDriver().get(ConfigReader.getProperty("hmcUrl"));
         HmcPage hotelMyCampPage = new HmcPage();
+
         /*Page class'ındaki locate'lerimize ulaşabilmek için
         Page classımızdan bir obje oluşturarak, oluşturmuş olduğumuz obje ile
         page classımızdaki locate'lerimize ulaşabiliriz
          */
+
         hotelMyCampPage.login.click();
         //test data username: manager ,
         // test data password : Manager1!
